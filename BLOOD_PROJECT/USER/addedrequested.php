ce562c2fd247cfd1fd2a9067210533c97cb51e97
BIJJAM RAMA SAI MAHESWARA REDDY <111236831+BIJJAMRAMASAIMAHESWARAREDDY@users.noreply.github.com>
2024-06-09 12:02:54 +0530

    Add files via upload

---
+++ BLOOD_PROJECT/USER/addedrequested.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>BDMS</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="../icofont/icofont.min.css">
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+    <?php include 'includes/donornav.php'?>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">BBMS</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            MESSAGE BOX
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <form role="form" action="index.php" method="post">
+							<?php 
+
+
+
+    $bloodgroup = $_POST["bloodgroup"];
+    $address = $_POST["address"];
+    
+    $quantity = $_POST["quantity"];
+    include '../pages/dbconnect.php';
+    //code after connection is successfull
+    $qry = "insert into bloodrequest(bloodgroup,address,quantity) values ('$bloodgroup','$address','$quantity')";
+    $result = mysqli_query($conn,$qry); //query executes
+
+    if(! $result){
+        echo"ERROR";
+    }else {
+        echo" <div style='text-align: center'><h1>Blood requested. Thank You.</h1>";
+        echo" <a href='userdashboard.php' div style='text-align: center'><h3>Go Back</h3>";
+
+    }
+
+
+?>
+
+</form>
+                                </div>
+                                
+                            </div>
+                            <!-- /.row (nested) -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+
+</body>
+
+<footer>
+        <p>&copy; <?php echo date("Y"); ?>: BIJJAM RAMA SAI MAHESWARA REDDY</p>
+    </footer>
+	
+	<style>
+	footer{
+   background-color: #424558;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 35px;
+    text-align: center;
+    color: #CCC;
+}
+
+footer p {
+    padding: 10.5px;
+    margin: 0px;
+    line-height: 100%;
+}
+	</style>
+
+</html>
