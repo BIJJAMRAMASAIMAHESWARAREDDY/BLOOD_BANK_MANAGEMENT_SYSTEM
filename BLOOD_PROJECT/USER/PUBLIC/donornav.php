8acab67142a3f423d039033692fa27e3b1ec0297
BIJJAM RAMA SAI MAHESWARA REDDY <111236831+BIJJAMRAMASAIMAHESWARAREDDY@users.noreply.github.com>
2024-06-09 12:08:00 +0530

    Add files via upload

---
+++ BLOOD_PROJECT/USER/PUBLIC/donornav.php
@@ -0,0 +1,77 @@
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php"><i class="icofont-blood-drop"></i>Blood Donor Management System</a>
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+               
+             
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <!-- <li class="divider"></li> -->
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+               
+                <!-- /.dropdown -->
+            </ul>
+            
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="userdashboard.php"><i class="fa fa-dashboard fa-fw"></i> Donor's Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="userviewblood.php"><i class="fa fa-heartbeat"></i>  View Blood Collections </a>
+                      
+                            <!-- /.nav-second-level -->
+                        </li>
+            
+                        
+                        <li>
+                            <a href="userviewannouncement.php"><i class="fa fa-bullhorn"></i> View Announcements </a>
+                     
+                        </li>
+
+                        <li>
+                            <a href="userviewcampaigns.php"><i class="fa fa-flag"></i> View Campaigns </a>
+                           
+                        </li>
+                        <li>
+                            <a href="addrequest.php"><i class="fa fa-flag"></i> request blood </a>
+                           
+                        </li>
+                        <li>
+                            <a href="view request.php"><i class="fa fa-flag"></i> View requested details </a>
+                           
+                        </li>
+            
+                        
+                       
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
